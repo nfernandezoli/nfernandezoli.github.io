cc5bf9838385f2771679585fdc9bcccc8ff52121
junquera <javier.junquera.sanchez@gmail.com>
2014-11-19 11:49:06 +0100

    added jquery-ui

--- script/js/main.js
+++ script/js/main.js
@@ -8,6 +8,12 @@ var main = function(){
 		$('#main').animate({top: "0"}, 1000);
 		$('#datos').css('position', 'fixed');
 	});
-	$('#saber_mas').mouseover(function(){document.body.style.cursor='pointer';}).mouseout(function(){document.body.style.cursor='auto';});
+	$('#saber_mas').mouseover(function(){
+				$('#saber_mas').animate({color: "#DFDFDF"}, 200);
+				/*document.body.style.cursor='pointer'; */
+			}).mouseout(function(){
+				$('#saber_mas').animate({color: "#FFFFFF"}, 200);
+				/*document.body.style.cursor='auto'; */
+			});
 };
 $(document).ready(main);
